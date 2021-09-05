fe349aa6266d3a72cd4278d2d744754f67076e58
Martin <mr.mmaurer+work@gmail.com>
2021-09-05 21:53:39 +0100

    Added v-for/v-if precedence example

---
+++ code/examples/vue2-v-for-v-if-1/component.ts
@@ -0,0 +1,44 @@
+import Vue from 'vue2';
+
+export default Vue.extend({
+  data() {
+    return {
+      people: [
+        {
+          name: 'Cersei Centenarian',
+          age: 100,
+        },
+        {
+          name: 'Ted Tenner',
+          age: 10,
+        },
+        {
+          name: 'Franklin Fiver',
+          age: 5,
+        },
+      ],
+    };
+  },
+  computed: {
+    filteredPeople() {
+      return this.people.filter((person) => person.age > 5);
+    },
+  },
+  template: `
+    <div class="root">
+      <dl v-for="person in people" v-if="person.age > 5">
+        <dt>Name: </dt>
+        <dd>{{ person.name }}</dd>
+        <dt>Age: </dt>
+        <dd>{{ person.age }}</dd>
+      </dl>
+      <hr />
+      <dl v-for="person in filteredPeople">
+        <dt>Name: </dt>
+        <dd>{{ person.name }}</dd>
+        <dt>Age: </dt>
+        <dd>{{ person.age }}</dd>
+      </dl>
+    </div>
+  `,
+});
